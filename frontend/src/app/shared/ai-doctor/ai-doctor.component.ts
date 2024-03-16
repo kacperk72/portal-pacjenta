cec3d32f7675daa3d799944ca3fae74cc2d6bafa
Kacper Kubit <kacper.kubit99@gmail.com>
2024-03-16 10:03:03 +0100

    ai doctor structure

--- frontend/src/app/shared/ai-doctor/ai-doctor.component.ts
+++ frontend/src/app/shared/ai-doctor/ai-doctor.component.ts
@@ -18,4 +18,15 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './ai-doctor.component.html',
   styleUrl: './ai-doctor.component.css',
 })
-export class AiDoctorComponent {}
+export class AiDoctorComponent {
+  description: string = '';
+  aiResponseSick: string =
+    'Według twoich objawów masz przeziębienie, zaleca się wzięcie paracetamolu i pozostanie w domu przez 3 dni';
+  aiResponseHealth =
+    'Według twoich parametrów twój wskaźnik BMI wynosi ..., zaleca się zwiększenie aktywności i zdrowszą dietę. Zaleca się ograniczenie spożycia tłuszczy nasyconych oraz zwiększony udział warzyw w diecie';
+  userParams = {
+    gender: '',
+    age: '0',
+    weight: '0',
+  };
+}
